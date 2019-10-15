71706fe0bc6acb5f146147588783695d530c5477
Ryeong-hee <56580286+Ryeong-hee@users.noreply.github.com>
2019-10-15 18:53:20 +0900

    Pull up

--- Basics/Button/Button.ino
+++ Basics/Button/Button.ino
@@ -1,14 +1,14 @@
-r int buttonPin = 2;     
+int buttonPin = 2;     
 const int ledPin =  13;      
 int buttonState = 0;         
 void setup() {
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
-}
+  pinMode(buttonPin, INPUT_PULLUP); //pullup ; digital in -> button -> ground
+}                                 
 void loop() {
   buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH);
+  if (buttonState ==LOW) {  //버튼 누르면 ground로 (=low) low일 때 HIGH
+    digitalWrite(ledPin,HIGH);
   } else {
     digitalWrite(ledPin, LOW);
   }
